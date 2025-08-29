b9c2758e856abe3ea5c21275eb1c29dcd610ef15
FJ <felipegd.jr@gmail.com>
2025-08-29 16:44:56 -0300

    add more tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,11 @@
 import requests
 
-BASE_URL = "https://man.kontrl.tech/kore/rest/healthcheck/"
-HEADERS = {"Content-Type": "application/json"}
+BASE_URL = "https://hel.kore.kontrl.tech/health-check"
+HEADERS = {
+"Content-Type": "application/json",
+"x-kore-drone-uid":"drone:87c0976259b402a4160bff6db091e14bfcb9d0ea3f1e3425ef073ad146e2b13e",
+"x-kore-drone-sign": ""
+}
 
 def test_healthcheck_all_modules_ok():
     payload = {"modules": "all"}
@@ -11,18 +15,14 @@ def test_healthcheck_all_modules_ok():
     data = response.json()
     assert isinstance(data, dict), "A resposta deve ser um JSON com módulos"
     for module, status in data.items():
+        if module == "version": continue
         assert status.lower() == "ok", f"Módulo {module} não está saudável (status: {status})"
 
-def test_healthcheck_some_module_nok():
+def test_healthcheck_some_modules():
     payload = {"modules": ["drone"]}
     response = requests.post(BASE_URL, json=payload, headers=HEADERS, timeout=10)
-    assert response.status_code in (200, 504)
+    assert response.status_code == 200
 
     data = response.json()
     assert "drone" in data
     assert data["drone"].lower() in ("ok", "nok")
-
-def test_healthcheck_timeout():
-    payload = {"modules": ["drone"]}
-    response = requests.post(BASE_URL, json=payload, headers=HEADERS, timeout=5)
-    assert response.status_code in (200, 504)
